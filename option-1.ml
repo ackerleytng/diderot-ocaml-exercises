8a84ea34249f017bae18bc4153e112bf31d1b3d2
Ackerley Tng <ackerleytng@gmail.com>
2016-12-12 19:42:59 +0800

    Halfway through writing solve'

--- option-1.ml
+++ option-1.ml
@@ -25,7 +25,7 @@ let rec flat_map (f : 'e rel) : 'e list -> 'e list = function
   | x :: xs -> (f x) @ flat_map f xs
   
 let rec iter_rel (rel : 'e rel) (times : int) : 'e rel =
-  if times <= 1 then rel
+  if times < 1 then (fun x -> [x])
   else fun x -> flat_map rel (iter_rel rel (times - 1) x)
 
 type 'e prop = 'e -> bool
@@ -40,8 +40,25 @@ let solve (r : 'a rel) (p : 'a prop) (x : 'a) =
   aux [x]
 
 let solve_path (r : 'a rel) (p : 'a prop) (x : 'a) =
-  let solution = solve r p x in
-  let rec aux level = 
-    solve near (fun k -> exists p (iter_rel r level k)) x
+  let rec aux accum level =
+    let solution = solve r (fun k -> exists p (iter_rel r level k)) x in
+    if solution = x then solution :: accum
+    else aux (solution :: accum) (level + 1)
   in
-  aux 0
+  aux [] 0
+
+type ('a, 'set) set_operations =
+  { empty : 'set ;
+    mem : 'a -> 'set -> bool ;
+    add : 'a -> 'set -> 'set }
+
+let archive_map (opset : ('a, 'set) set_operations) (r : 'a rel) (s, l) =
+  let results = flat_map r l in
+  let l' = List.filter (fun e -> not (opset.mem e s)) results in
+  let s' = List.fold_left (fun s e -> opset.add e s) s l' in
+  (s', l')
+
+let solve' (opset : ('a, 'set) set_operations) (r : 'a rel) p x =
+  let rec aux (s, l) =
+    if opset.mem x s then x
+    else archive_map opset r 
