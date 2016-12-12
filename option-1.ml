b98737f218b309fbcfa2b6eecc62ba0c05af9431
Ackerley Tng <ackerleytng@gmail.com>
2016-12-13 06:31:41 +0800

    Halfway through writing solve_path'

--- option-1.ml
+++ option-1.ml
@@ -30,6 +30,13 @@ let rec iter_rel (rel : 'e rel) (times : int) : 'e rel =
 
 type 'e prop = 'e -> bool
              
+(* 
+ * solve takes a generator function, a checker function, and an initial state
+ *
+ * In each step, the checker function is applied on the possible states.
+ * If the solution is among the possible states, the problem is solved.
+ * Otherwise, the generator is applied and the possible states are checked again.
+ *)
 let solve (r : 'a rel) (p : 'a prop) (x : 'a) =
   let rec aux list =
     try
@@ -39,6 +46,20 @@ let solve (r : 'a rel) (p : 'a prop) (x : 'a) =
   in
   aux [x]
 
+(*
+ * solve_path takes a generator function, a checker function, and an initial state
+ *
+ * In each step, we try to solve a question.
+ * In the initial step, the question is:
+ *   What is the answer if you iteratively apply the generator 0 times (identity function)
+ *   This will get us the final solution
+ * In the next step, the question is:
+ *   What is the answer if you apply the generator once?
+ * Then, 
+ *   _____________________________________________ twice?
+ * And so on.
+ * In this way, the path is built up.
+ *)
 let solve_path (r : 'a rel) (p : 'a prop) (x : 'a) =
   let rec aux accum level =
     let solution = solve r (fun k -> exists p (iter_rel r level k)) x in
@@ -58,7 +79,40 @@ let archive_map (opset : ('a, 'set) set_operations) (r : 'a rel) (s, l) =
   let s' = List.fold_left (fun s e -> opset.add e s) s l' in
   (s', l')
 
+(* 
+ * solve' takes a set of functions for manipulating a set of elements, 
+ *   a generator function, a checker function, and an initial state
+ *
+ * In each step, the checker function is applied on the 
+ *   POSSIBLE new states that have NEVER BEEN CHECKED.
+ * If the solution is among those states, the problem is solved.
+ * Otherwise, the generator is applied and checking is done again.
+ *)
 let solve' (opset : ('a, 'set) set_operations) (r : 'a rel) p x =
   let rec aux (s, l) =
-    if opset.mem x s then x
-    else archive_map opset r 
+    try
+      find p l
+    with
+      NotFound -> aux (archive_map opset r (s, l))
+  in
+  aux (opset.empty, [x])
+
+(* 
+ * solve_path' takes a set of functions for manipulating a list of elements,
+ *   a generator function, a checker function, and an initial state
+ *
+ * I suspect the element list in the set are paths that have been checked before,
+ *   so the set is actually used to constrain the paths chosen
+ *
+ * In each step, the checker function is applied on the 
+ *   POSSIBLE new states that have NEVER BEEN CHECKED.
+ * If the solution is among those states, the problem is solved.
+ * Otherwise, the generator is applied and checking is done again.
+ *)
+let solve_path' (opset : ('a list, 'set) set_operations) (r : 'a rel) p x =
+  let rec aux accum level =
+    let solution = solve' opset r (fun k -> exists p (iter_rel r level k)) x in
+    if solution = x then solution :: accum
+    else aux (solution :: accum) (level + 1)
+  in
+  aux [] 0
